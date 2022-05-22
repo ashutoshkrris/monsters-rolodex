e0e3cd3056382014db48da6f97191fe510092559
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-05-22 22:12:45 +0530

    Keys for Mapping

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ class App extends Component {
     this.state = {
       monsters: [
         {
+          id: 1,
           name: "Linda",
         },
         {
+          id: 2,
           name: "Frank",
         },
         {
+          id: 3,
           name: "Sally",
         },
         {
+          id: 4,
           name: "Jacky",
         },
         {
+          id: 5,
           name: "Bhagnani",
         },
       ],
@@ -31,7 +36,11 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
